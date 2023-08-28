e71e4a3888c7209d038c466ee3d064aaec128cbe
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-08-27 23:28:31 -0300

    making my classifier

--- classifier.py
+++ classifier.py
@@ -1,2 +1,24 @@
 from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import scipy
 
+# Serão 8 classificadores, 1 para cada lead. A moda das classificações vai ser a classificação final
+
+
+class MyClassifier:
+    def __init__(self, n_neighbors: int) -> None:
+        self.clfs = [
+            KNeighborsClassifier(n_neighbors=5, n_jobs=-1, metric="precomputed")
+            for _ in range(8)
+        ]
+
+    # X e y são 3d e 1d, respectivamente. Contêm as distâncias entre cada lead, mas as classes são a mesma pra todas
+    def fit(self, X, y):
+        for i in range(8):
+            self.clfs[i].fit(X[i], y)
+
+    def predict(self, X):
+        pred = np.zeros(8)
+        for i, clf in enumerate(self.clfs):
+            pred[i] = clf.predict(X)
+        return scipy.stats.mode(pred)
